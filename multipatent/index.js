fe313d3493b0e0ea37269f7d7f9cd392cbbdf2c6
emrahkilic <kilic.emrah@gmail.com>
2020-08-25 17:33:27 +0200

    updated

--- multipatent/index.js
+++ multipatent/index.js
@@ -1,5 +1,29 @@
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+
 
 window.addEventListener('scroll', function() {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
-  });
+    });
+
+} else {
+
+    const menu = document.querySelector('.nav-links');
+
+    menu.addEventListener('mouseover', () => {
+      menu.classList.add('show-menu')
+    })
+
+    menu.addEventListener('mouseleave', () => {
+      menu.classList.remove('show-menu')
+    })
+
+
+    
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 720px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
