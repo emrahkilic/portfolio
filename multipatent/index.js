75d3267123699cee1086018e3946ceaa5573f461
emrahkilic <kilic.emrah@gmail.com>
2020-08-26 14:42:01 +0200

    updated

--- multipatent/index.js
+++ multipatent/index.js
@@ -1,29 +1,36 @@
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-
 
 window.addEventListener('scroll', function() {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
     });
 
-} else {
+//////
 
-    const menu = document.querySelector('.nav-links');
+// Select the elements we want to show or hide
+var burger = document.querySelector('burger');
+var navList = document.querySelector('nav-links');
+var closeBtn = document.querySelector('close');
+var overlay = document.querySelector('overlay');
 
-    menu.addEventListener('mouseover', () => {
-      menu.classList.add('show-menu')
-    })
+// When the user clicks on the hamburger icon the 'open' class is added
+// to both the menu and overlay elements making them visible and triggering the animation
+burger.addEventListener('click', function(){
+    navList.className += ' open';
+    overlay.className += ' open';
+})
 
-    menu.addEventListener('mouseleave', () => {
-      menu.classList.remove('show-menu')
-    })
+// When the close button is clicked the 'open' class is removed from
+// both the menu and overlay elements making them invisible
+closeBtn.addEventListener('click', function(){
+    navList.className = 'nav-links';
+    overlay.className = 'overlay';
+})
 
-
-    
+// If a user clicks outside the menu on the overlay it will also trigger the event
+// to hide the menu and overlay elements
+window.addEventListener('click', function(event){
+    if(event.target === overlay){
+      navList.className = 'nav-Links';
+      overlay.className = 'overlay';
     }
-  }
-  
-  var x = window.matchMedia("(min-width: 720px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes
+  })
